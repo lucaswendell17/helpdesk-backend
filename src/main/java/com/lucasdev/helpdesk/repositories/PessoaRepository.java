674a84434fada679e470644248ff6dee76459bbd
Lucas Wendel <lucas.wmartins@hotmail.com>
2023-05-21 20:33:13 -0300

    Excecao para CPF e Email já cadastrados

--- src/main/java/com/lucasdev/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/lucasdev/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.lucasdev.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lucasdev.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
